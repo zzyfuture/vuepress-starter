82cf56f0501bf6c38c9f6676b93a4d0e68187888
zzyfuture <24202113@qq.com>
2025-02-20 13:45:36 +0800

    add gitignore, add dist

--- docs/.vuepress/.temp/pages/tag/tag-b/index.html.js
+++ docs/.vuepress/.temp/pages/tag/tag-b/index.html.js
@@ -1,16 +1,3 @@
-import comp from "C:/Users/ZZY/PycharmProjects/vuepress-starter/vuepress-starter/docs/.vuepress/.temp/pages/tag/tag-b/index.html.vue"
+import comp from "C:/Users/ZZY/PycharmProjects/vuepress-starter/docs/.vuepress/.temp/pages/tag/tag-b/index.html.vue"
 const data = JSON.parse("{\"path\":\"/tag/tag-b/\",\"title\":\"Tag tag B\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Tag tag B\",\"sidebar\":false,\"blog\":{\"type\":\"category\",\"name\":\"tag B\",\"key\":\"tag\"},\"layout\":\"Tag\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/tag/wwi/index.html.js
+++ docs/.vuepress/.temp/pages/tag/wwi/index.html.js
@@ -1,16 +1,3 @@
-import comp from "C:/Users/ZZY/PycharmProjects/vuepress-starter/vuepress-starter/docs/.vuepress/.temp/pages/tag/wwi/index.html.vue"
+import comp from "C:/Users/ZZY/PycharmProjects/vuepress-starter/docs/.vuepress/.temp/pages/tag/wwi/index.html.vue"
 const data = JSON.parse("{\"path\":\"/tag/wwi/\",\"title\":\"Tag WWI\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Tag WWI\",\"sidebar\":false,\"blog\":{\"type\":\"category\",\"name\":\"WWI\",\"key\":\"tag\"},\"layout\":\"Tag\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
