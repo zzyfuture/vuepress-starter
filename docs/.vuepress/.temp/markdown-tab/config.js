690145fa4bd1cecf939d67a413959f5d55c25092
zzyfuture <24202113@qq.com>
2025-02-20 13:16:37 +0800

    add dist

--- docs/.vuepress/.temp/markdown-tab/config.js
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -1,6 +1,6 @@
-import { CodeTabs } from "C:/Users/ZZY/PycharmProjects/vuepress-starter/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
-import { Tabs } from "C:/Users/ZZY/PycharmProjects/vuepress-starter/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
-import "C:/Users/ZZY/PycharmProjects/vuepress-starter/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+import { CodeTabs } from "C:/Users/ZZY/PycharmProjects/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "C:/Users/ZZY/PycharmProjects/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "C:/Users/ZZY/PycharmProjects/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
 
 export default {
   enhance: ({ app }) => {
--- docs/.vuepress/.temp/pages/tag/wwii/index.html.js
+++ docs/.vuepress/.temp/pages/tag/wwii/index.html.js
@@ -1,16 +1,3 @@
-import comp from "C:/Users/ZZY/PycharmProjects/vuepress-starter/vuepress-starter/docs/.vuepress/.temp/pages/tag/wwii/index.html.vue"
+import comp from "C:/Users/ZZY/PycharmProjects/vuepress-starter/docs/.vuepress/.temp/pages/tag/wwii/index.html.vue"
 const data = JSON.parse("{\"path\":\"/tag/wwii/\",\"title\":\"Tag WWII\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Tag WWII\",\"sidebar\":false,\"blog\":{\"type\":\"category\",\"name\":\"WWII\",\"key\":\"tag\"},\"layout\":\"Tag\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
